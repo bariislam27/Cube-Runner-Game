fd7a14b25c956e43e229536148fea95027e03082
Albari Islam <albari27@gmail.com>
2021-02-05 12:02:03 -0330

    Score board implemented

---
+++ Assets/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    void Update()
+    {
+        scoreText.text = (player.position.z + 498).ToString("0"); // z of player is -498, so the score will start from 000
+    }
+}
